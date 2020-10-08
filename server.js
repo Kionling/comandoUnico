970593516bb2a86f3b7a31b2e8f3b86c11cae55d
Kionling <djvdjv209@gmail.com>
2020-10-08 10:36:43 -0700

    established basic server configuration

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-va 
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 4000;
+
+app.use(express.urlencoded({ extended: true}))
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+  app.listen(PORT, () => {
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  });
