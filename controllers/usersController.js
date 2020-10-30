2f077a69c60fdbd4fda862b182aa47f9603107d9
Kionling <djvdjv209@gmail.com>
2020-10-30 14:28:38 -0700

    Created backend server functionality to handle the user login

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,19 +4,29 @@ module.exports = {
   signup(req, res) {
     db.Grupo.create({
       name: req.body.name + " Member of Comando Unico.",
-    }).then((newGrupo) => {
-      return db.User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        GrupoId: newGrupo.id,
-        color: req.body.color,
-      });
     })
-    .then(() => {
+      .then((newGrupo) => {
+        return db.User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+          GrupoId: newGrupo.id,
+          color: req.body.color,
+        });
+      })
+      .then(() => {
         res.redirect(307, "/api/users/login");
-    }).catch(err => {
-        res.status(401).(err.name);
-    })
+      })
+      .catch((err) => {
+        res.status(401).send(err.name);
+      });
   },
+  login(req, res) {
+      if (req.user) {
+          res.status(200).end();
+      } 
+      else {
+          res.status(401).end();
+      }
+  }
 };
