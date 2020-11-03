944431c8ccc9e485199d8abe52605ed9efee3b1f
Kionling <djvdjv209@gmail.com>
2020-11-03 11:34:39 -0800

    Generated Create, Update, Delete queries

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,40 +1,45 @@
 const db = require("../models");
 
 module.exports = {
-    getUserInfo(req, res) {
-        if(!req.user) {
-            return req.status(401).end();
-            //unauthorized server response if the correct user is not signed in 
-        }
-        db.Grupo.findOne({
-            where: { id: req.user.GrupoId},
-            include: db.User,
-
-        })
-        .then( grupo => {
-            return res.json({
-                name: grupo.name,
-                invite_code: grupo.invite_code,
-                members: grupo.Users.map(user => ({ id: user.id, name: user.name, color: user.color}))
-            })
-        })
-        .catch( err => {
-            console.log(err)
-            res.status(500).end()
-        })
-    },
-    getMembers(req,res) {
-        db.User.findAll({ where: { GrupoId: req.user.GrupoId}})
-        .then( data => res.json( 
-            data.map((row) => ({
-                id: row.dataValues.id, 
-                name: row.dataValues.name,
-                color: row.dataValues.color
-            }))
-        ))
-        .catch(err => {
-            console.log(err)
-            res.status(500).end();
-        })
+  getUserInfo(req, res) {
+    if (!req.user) {
+      return req.status(401).end();
+      //unauthorized server response if the correct user is not signed in
     }
-}
+    db.Grupo.findOne({
+      where: { id: req.user.GrupoId },
+      include: db.User,
+    })
+      .then((grupo) => {
+        return res.json({
+          name: grupo.name,
+          invite_code: grupo.invite_code,
+          members: grupo.Users.map((user) => ({
+            id: user.id,
+            name: user.name,
+            color: user.color,
+          })),
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+      });
+  },
+  getMembers(req, res) {
+    db.User.findAll({ where: { GrupoId: req.user.GrupoId } })
+      .then((data) =>
+        res.json(
+          data.map((row) => ({
+            id: row.dataValues.id,
+            name: row.dataValues.name,
+            color: row.dataValues.color,
+          }))
+        )
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).end();
+      });
+  },
+};
---
+++ controllers/performanceController.js
@@ -0,0 +1,43 @@
+const { where } = require("sequelize/types");
+const db = require("../models");
+
+module.exports = {
+  create(req, res) {
+    db.Performance.create({
+      Performance: req.body.Performance,
+      GrupoId: req.user.GrupoId,
+      UserId: req.body.UserId,
+    })
+      .then(() => {
+        res.status(200).end();
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
+  update(req, res) {
+    db.Performance.update(
+      {
+        Performance: req.body.Performance,
+        UserId: req.body.UserId,
+      },
+      {
+        where: { id: req.query.id },
+      }
+    )
+      .then(() => {
+        res.status(200).end();
+      })
+      .catch((err) => {
+        res.status(500).end();
+      });
+  },
+  delete(req, res) {
+    db.Performance.destroy({ where: { id: req.query.id } })
+    .then(() => {
+        res.status(200).end();
+    }).catch(err => {
+        res.status(500).end();
+    })
+  },
+};
