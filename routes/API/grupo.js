e6d8e0ed63b99985eca368911d0d958c57d9b5a4
Kionling <djvdjv209@gmail.com>
2020-11-08 10:27:09 -0800

    Updated all API routes as well as the schema for the database.

--- routes/API/grupo.js
+++ routes/API/grupo.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const grupoController = require("../../controllers/groupController.js");
+const grupo = require("../../models/grupo.js");
 
 router.route("/").get(grupoController.getUserInfo)
 router.route("/users").get(grupoController.getMembers)
 router.route("/getInviteCode").get(grupoController.getInviteCode)
+router.route("/joinGrupo").get(grupoController.joinGrupo)
