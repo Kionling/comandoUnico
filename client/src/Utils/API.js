73b443b25f2a5f1a9b83feeaac01b27d07c55957
Kionling <djvdjv209@gmail.com>
2020-11-19 14:27:32 -0800

    Updated the global state with a new performance context for front/backend util

--- client/src/Utils/API.js
+++ client/src/Utils/API.js
@@ -1,13 +1,35 @@
 import Axios from "axios"
+// import { getUserInfo } from "../../../controllers/groupController"
 
 export default {
-    login: function() {
-        return Axios.post("/api/users/login")
+    login: function(email, password) {
+        return Axios.post("/api/users/login", {email, password})
     },
-    signUp: function() {
-        return Axios.post("/api/users/signUp")
+    signUp: function(name, email, password) {
+        return Axios.post("/api/users/signUp", {name, email, password})
     },
     getMembers: function(){
         return Axios.get("/api/Grupo/users")
+    },
+    getUserInformation: function(){
+        return Axios.get("/api/users")
+    },
+    joinGrupo: function(inviteCode){
+        return Axios.put("/api/Grupo/join?=invite"+ inviteCode)
+    },
+    createPerformance: function(performance, UserId, date){
+        return Axios.post("/api/performances", {performance ,UserId, date})
+    },
+    removePerformance: function(performanceId){
+        return Axios.delete("/api/performance?id=" + performanceId)
+    },
+    getGrupoinfo: function(){
+        return Axios.get("/api/grupo")
+    },
+    getGrupoPerformances: function(){
+        return Axios.get("/api/performances")
+    },
+    logout: function(){
+        return Axios.get("/api/users/logout")
     }
 }
